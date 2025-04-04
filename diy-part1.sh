3d40f9d6a83408c100665c181e70fe21b8b6f37a
jgsports <jgsports@gmail.com>
2025-04-04 10:15:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
